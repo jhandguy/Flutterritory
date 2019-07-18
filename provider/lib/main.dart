3c9617819350e7f1f120aea1043ddcd34fb3c1ce
Jean Mainguy <9969006+jhandguy@users.noreply.github.com>
2019-07-18 09:41:20 +0200

    Add Provider project

---
+++ provider/lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    Flutteritory(
+      title: 'Flutteritory',
+    )
+  );
+}
+
+class Flutteritory extends StatelessWidget {
+  final String title;
+
+  Flutteritory({Key key, this.title}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: title,
+      theme: ThemeData.dark(),
+      home: Center(
+        child: Text(title),
+      ),
+    );
+  }
+}
