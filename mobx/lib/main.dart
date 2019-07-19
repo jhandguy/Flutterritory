31ddd879d3b16ac9dae22c3d6b90d115edffd2cc
Jean Mainguy <9969006+jhandguy@users.noreply.github.com>
2019-07-19 15:32:19 +0200

    Add MobX project

---
+++ mobx/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    Flutteritory(
+      title: 'Flutteritory',
+    )
+  );
+}
+
+class Flutteritory extends StatelessWidget {
+  final String title;
+
+  Flutteritory({Key key, this.title}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(title);
+  }
+}
